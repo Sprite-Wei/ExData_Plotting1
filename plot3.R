76aa8571b960e086df754dd4d49b72cdcf3d6bb4
Weiwei Han <hanweiwei7@163.com>
2016-02-08 21:30:17 +0800

    minor revise

--- plot3.R
+++ plot3.R
@@ -2,7 +2,7 @@ power <- read.table("household_power_consumption.txt",
                     header = TRUE, sep = ";",
                     colClasses = "character")
 pow <- subset(power, Date == "1/2/2007" | Date == "2/2/2007")
-datetime = strptime(paste(pow[,1],pow[,2]), "%d/%m/%Y %H:%M:%S")
+datetime <- strptime(paste(pow[,1],pow[,2]), "%d/%m/%Y %H:%M:%S")
 png("plot3.png")
 plot(datetime, as.numeric(pow[,7]), type = "l", 
      xlab= "", ylab = "Energy sub metering")
--- plot4.R
+++ plot4.R
@@ -2,7 +2,7 @@ power <- read.table("household_power_consumption.txt",
                     header = TRUE, sep = ";",
                     colClasses = "character")
 pow <- subset(power, Date == "1/2/2007" | Date == "2/2/2007")
-datetime = strptime(paste(pow[,1],pow[,2]), "%d/%m/%Y %H:%M:%S")
+datetime <- strptime(paste(pow[,1],pow[,2]), "%d/%m/%Y %H:%M:%S")
 png("plot4.png")
 par(mfcol = c(2,2))
 plot(datetime, as.numeric(pow[,3]), type = "l", 
